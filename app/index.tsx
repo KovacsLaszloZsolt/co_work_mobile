1e7762b5a8bd4b006ee294b2e71e06b190b76454
Laszlo Kovacs <laszlo.kovacs@tappointment.com>
2025-01-25 00:26:51 +0000

    CW-init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text className="font-extrabold text-red-800">HELLO00000</Text>
+    </View>
+  );
+}
